1fd1a088043310eb66003db590004e6ee080996f
DuongXuanBinh <64977863+DuongXuanBinh@users.noreply.github.com>
2021-03-25 17:17:13 +0700

    email blade

---
+++ seatmap_320.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/jquery-ui.min.js"></script>
+</head>
+<body>
+    <table>
+        
+        <tr>
+            <td>A</td>
+            <td>B</td>
+            <td></td>
+            <td>C</td>
+            <td>D</td>
+        </tr>
+        <tr>
+            <td>
+                <label for="1a">1A</label>
+                <input type="checkbox" id="1a" value="1a">
+            </td>
+            <td><label for="1b">1B</label>
+                <input type="checkbox" id="1b" value="1b">
+            </td>
+            <td>1</td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+</body>
+</html>
