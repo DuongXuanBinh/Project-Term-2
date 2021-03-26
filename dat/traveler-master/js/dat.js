8f0f1e9add562aca104e74eb0de2bc8b1584d5fb
datnguyen130 <65001229+datnguyen130@users.noreply.github.com>
2021-03-26 12:00:47 +0700

    dat
    dat

--- dat/traveler-master/js/dat.js
+++ dat/traveler-master/js/dat.js
@@ -5,19 +5,5 @@ $(function () {
             $(this).addClass('selected');
     })
 });
-$(function() {
-    $.datepicker.setDefaults({
-        showOn: "both",
-        buttonImageOnly: true,
-        buttonImage: "calendar.gif",
-        buttonText: "Calendar"
-    });
 
-    $("#from-outbound-date").click(function(){ $(this).datepicker.setDefaults({
-        showOn: "both",
-        buttonImageOnly: true,
-        buttonImage: "calendar.gif",
-        buttonText: "Calendar"
-    }); });
 
-});
