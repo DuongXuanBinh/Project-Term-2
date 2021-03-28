1dea0174a8bdaaec2c88b4616c0643ece11c6cc5
DuongXuanBinh <64977863+DuongXuanBinh@users.noreply.github.com>
2021-03-28 09:39:08 +0700

    a

---
+++ seat-320.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<!--business-->
+<table>
+    <tr>
+        <td>A</td>
+        <td>B</td>
+        <td></td>
+        <td>C</td>
+        <td>D</td>
+    </tr>
+    <?php
+    for ($row=1;$row<5;$row++){
+        echo "<tr>
+        <td>
+     <label for=>$row.A</label>   
+</td>
+</tr>";
+    }
+    ?>
+
+    <tr>
+        <td>A</td>
+        <td>B</td>
+        <td>C</td>
+        <td></td>
+        <td>D</td>
+        <td>E</td>
+        <td>G</td>
+    </tr>
+</table>
+<!--economy-->
+<table>
+
+</table>
+<!--savings-->
+<table>
+
+</table>
+</body>
+</html>
